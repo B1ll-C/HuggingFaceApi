c416b779164a2a5759444a8094e6cb45d62e5804
Billy <111455157+B1ll-C@users.noreply.github.com>
2024-07-27 22:48:58 +0800

    Routes added

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/',(req,res)=>{
   res.send(200)
 })
 
-app.post('/upload', upload.single('image'), async (req, res) => {
+app.post('/Pest', upload.single('image'), async (req, res) => {
   try {
     // Ensure the file was uploaded
     if (!req.file) {
@@ -41,6 +41,62 @@ app.post('/upload', upload.single('image'), async (req, res) => {
     res.status(500).send('An error occurred');
   }
 });
+
+app.post('/Disease', upload.single('image'), async (req, res) => {
+  try {
+    // Ensure the file was uploaded
+    if (!req.file) {
+      return res.status(400).send('No file uploaded.');
+    }
+
+    // Get the uploaded file
+    const imageBuffer = req.file.buffer;
+    const blob = new Blob([imageBuffer], { type: 'image/png' });
+
+    // Import Gradio client
+    const { Client } = await import('@gradio/client');
+    const client = await Client.connect("Thesis-GROOT/Disease");
+    const result = await client.predict("/predict", { 
+      image: blob,
+    });
+
+    // Log and send the result
+    console.log(result.data);
+    res.json(result.data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('An error occurred');
+  }
+});
+
+
+// app.post('/Pest', upload.single('image'), async (req, res) => {
+//   try {
+//     // Ensure the file was uploaded
+//     if (!req.file) {
+//       return res.status(400).send('No file uploaded.');
+//     }
+
+//     // Get the uploaded file
+//     const imageBuffer = req.file.buffer;
+//     const blob = new Blob([imageBuffer], { type: 'image/png' });
+
+//     // Import Gradio client
+//     const { Client } = await import('@gradio/client');
+//     const client = await Client.connect("Thesis-GROOT/Pest");
+//     const result = await client.predict("/predict", { 
+//       image: blob,
+//     });
+
+//     // Log and send the result
+//     console.log(result.data);
+//     res.json(result.data);
+//   } catch (error) {
+//     console.error('Error:', error);
+//     res.status(500).send('An error occurred');
+//   }
+// });
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
